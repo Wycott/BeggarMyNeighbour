263513cd421cdc803394b8e3d632dee532722ff1
Wycott <robert.g.docherty@gmail.com>
2024-12-15 18:33:48 +0000

    Separate result code in main routine

--- BeggarMyNeighbour/Program.cs
+++ BeggarMyNeighbour/Program.cs
@@ -23,34 +23,43 @@ internal class Program
                 continue;
             }
 
-            record++;
-            maxCards = dealResult.Cards;
-            var maxDecks = dealResult.Cards / 52;
-
-            var resultLine1 = $"{record}) {DateTime.Now}";
-            var resultLine2 = $"After {iterations:N0} iterations";
-            var resultLine3 = $"Most cards played is {maxCards:N0}";
-            var resultLine4 = $"Most decks played is {maxDecks:N0}";
-            var resultLine5 = $"Most tricks played is {dealResult.Tricks:N0}";
-            var resultLine8 = string.Empty;
-
-            Output.WriteResults(fileNameStub,
-                resultLine1,
-                                resultLine2,
-                                resultLine3,
-                                resultLine4,
-                                resultLine5,
-                                dealResult.PlayerOneDeal,
-                                dealResult.PlayerTwoDeal,
-                                resultLine8);
+            maxCards = OutputBestResultSoFar(dealResult, iterations, fileNameStub, ref record);
         }
         // ReSharper disable once FunctionNeverReturns
+    }
+
+    private static int OutputBestResultSoFar(DealResult dealResult, int iterations, string fileNameStub, ref int record)
+    {
+        record++;
+        
+        var maxCards = dealResult.Cards;
+        var maxDecks = dealResult.Cards / 52;
+
+        var resultLine1 = $"{record}) {DateTime.Now}";
+        var resultLine2 = $"After {iterations:N0} iterations";
+        var resultLine3 = $"Most cards played is {maxCards:N0}";
+        var resultLine4 = $"Most decks played is {maxDecks:N0}";
+        var resultLine5 = $"Most tricks played is {dealResult.Tricks:N0}";
+        var resultLine8 = string.Empty;
+
+        Output.WriteResults(fileNameStub,
+            resultLine1,
+            resultLine2,
+            resultLine3,
+            resultLine4,
+            resultLine5,
+            dealResult.PlayerOneDeal,
+            dealResult.PlayerTwoDeal,
+            resultLine8);
+
+        return maxCards;
     }
 
     private static async Task<DealResult> NormalPlay()
     {
 
         var deal = await Deck.GenerateStacks();
+
         return await Engine.RunScenario(deal.PlayerOneDeal, deal.PlayerTwoDeal);
     }
 
