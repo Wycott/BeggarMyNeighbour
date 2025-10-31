0469259f7deb5e01c67077b7b70468448b08ec70
Wycott <robert.g.docherty@gmail.com>
2025-10-31 23:06:10 +0000

    Add another test and sort out a namespace issue

---
+++ BeggarMyNeighbourLibrary.Test/DealResultTest.cs
@@ -0,0 +1,39 @@
+﻿namespace BeggarMyNeighbourLibrary.Test;
+
+public class DealResultTest
+{
+    [Fact]
+    public void GenerateStacks_CreatesTwoStacksOfCards()
+    {
+        const int Tricks = 1164;
+        const int Cards = 8344;
+
+        const string PlayerOneDeal = "---AJ--Q---------QAKQJJ-QK";
+        const string PlayerTwoDeal = "-----A----KJ-K--------A---";
+
+        const int PlayerOneSuits = 11;
+        const int PlayerTwoSuits = 5;
+
+        const string PlayerOneOutcome = "Win";
+        const string PlayerTwoOutcome = "Lose";
+
+        var dealResult = new DealResult()
+        {
+            PlayerOneDeal = PlayerOneDeal,
+            PlayerTwoDeal = PlayerTwoDeal,
+            Cards = Cards,
+            Tricks = Tricks,
+            PlayerOneOutcome = PlayerOneOutcome,
+            PlayerTwoOutcome = PlayerTwoOutcome
+        };
+            
+        Assert.Equal(Tricks, dealResult.Tricks);
+        Assert.Equal(Cards, dealResult.Cards);
+        Assert.Equal(PlayerOneDeal, dealResult.PlayerOneDeal);
+        Assert.Equal(PlayerTwoDeal, dealResult.PlayerTwoDeal);
+        Assert.Equal(PlayerOneOutcome, dealResult.PlayerOneOutcome);
+        Assert.Equal(PlayerTwoOutcome, dealResult.PlayerTwoOutcome);
+        Assert.Equal(PlayerOneSuits, dealResult.PlayerOneSuits);
+        Assert.Equal(PlayerTwoSuits, dealResult.PlayerTwoSuits);
+    }
+}
--- BeggarMyNeighbourLibrary.Test/EngineTest.cs
+++ BeggarMyNeighbourLibrary.Test/EngineTest.cs
@@ -1,35 +1,34 @@
-﻿namespace BeggarMyNeighbourLibrary.Test
+﻿namespace BeggarMyNeighbourLibrary.Test;
+
+public class EngineTest
 {
-    public class EngineTest
+    [Fact]
+    public async void RunScenario_LongestRun_ReturnsExpectedTricksAndCards()
     {
-        [Fact]
-        public async void RunScenario_LongestRun_ReturnsExpectedTricksAndCards()
-        {
-            const int ExpectedTricks = 1164;
-            const int ExpectedCards = 8344;
+        const int ExpectedTricks = 1164;
+        const int ExpectedCards = 8344;
 
-            const string PlayerOneDeal = "---AJ--Q---------QAKQJJ-QK";
-            const string PlayerTwoDeal = "-----A----KJ-K--------A---";
+        const string PlayerOneDeal = "---AJ--Q---------QAKQJJ-QK";
+        const string PlayerTwoDeal = "-----A----KJ-K--------A---";
 
-            var res = await Engine.RunScenario(PlayerOneDeal, PlayerTwoDeal);
+        var res = await Engine.RunScenario(PlayerOneDeal, PlayerTwoDeal);
 
-            Assert.Equal(ExpectedTricks, res.Tricks);
-            Assert.Equal(ExpectedCards, res.Cards);
-        }
+        Assert.Equal(ExpectedTricks, res.Tricks);
+        Assert.Equal(ExpectedCards, res.Cards);
+    }
 
-        [Fact]
-        public async void RunScenario_2ndLongestRun_ReturnsExpectedTricksAndCards()
-        {
-            const int ExpectedTricks = 1106;
-            const int ExpectedCards = 7972;
+    [Fact]
+    public async void RunScenario_2ndLongestRun_ReturnsExpectedTricksAndCards()
+    {
+        const int ExpectedTricks = 1106;
+        const int ExpectedCards = 7972;
             
-            const string PlayerOneDeal = "----K---A--Q-A--JJA------J";
-            const string PlayerTwoDeal = "-----KK---------A-JK-Q-Q-Q";
+        const string PlayerOneDeal = "----K---A--Q-A--JJA------J";
+        const string PlayerTwoDeal = "-----KK---------A-JK-Q-Q-Q";
 
-            var res = await Engine.RunScenario(PlayerOneDeal, PlayerTwoDeal);
+        var res = await Engine.RunScenario(PlayerOneDeal, PlayerTwoDeal);
 
-            Assert.Equal(ExpectedTricks, res.Tricks);
-            Assert.Equal(ExpectedCards, res.Cards);
-        }
+        Assert.Equal(ExpectedTricks, res.Tricks);
+        Assert.Equal(ExpectedCards, res.Cards);
     }
 }
